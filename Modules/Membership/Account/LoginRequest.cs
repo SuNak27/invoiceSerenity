b1b5f33e5acf435703d91e3bd6af3b9e6565c81e
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-06-15 13:27:31 +0700

    Finish

--- Modules/Membership/Account/LoginRequest.cs
+++ Modules/Membership/Account/LoginRequest.cs
@@ -3,13 +3,13 @@ using Serenity.Services;
 
 namespace InvoiceKu.Membership
 {
-    [FormScript("Membership.Login")]
-    [BasedOnRow(typeof(Administration.Entities.UserRow), CheckNames = true)]
-    public class LoginRequest : ServiceRequest
-    {
-        [Placeholder("username... (default is admin)")]
-        public string Username { get; set; }
-        [PasswordEditor, Placeholder("password...(default is 1234567)"), Required(true)]
-        public string Password { get; set; }
-    }
+  [FormScript("Membership.Login")]
+  [BasedOnRow(typeof(Administration.Entities.UserRow), CheckNames = true)]
+  public class LoginRequest : ServiceRequest
+  {
+    [Placeholder("Username")]
+    public string Username { get; set; }
+    [PasswordEditor, Placeholder("******"), Required(true)]
+    public string Password { get; set; }
+  }
 }
